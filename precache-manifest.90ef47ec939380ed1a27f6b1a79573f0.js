3f3c9d233e9f50215baed660852b275892c89086
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2020-07-08 01:05:38 +0530

    Updates

---
+++ precache-manifest.90ef47ec939380ed1a27f6b1a79573f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3a2949319b62a46cda13babc0e96b7",
+    "url": "/reactminiproject/index.html"
+  },
+  {
+    "revision": "eee613bfc791bbb7ebcd",
+    "url": "/reactminiproject/static/css/main.a1661d4c.chunk.css"
+  },
+  {
+    "revision": "c646474dc35eda25f4c2",
+    "url": "/reactminiproject/static/js/2.19ae0534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactminiproject/static/js/2.19ae0534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee613bfc791bbb7ebcd",
+    "url": "/reactminiproject/static/js/main.4b872fa5.chunk.js"
+  },
+  {
+    "revision": "7740316d2f04aea3b2f1",
+    "url": "/reactminiproject/static/js/runtime-main.4302bf67.js"
+  }
+]);
